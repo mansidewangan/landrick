3693c5c2bef8541fad0257dcd73752d433c5cee0
Mansi Dewangan <110015762+mansidewangan@users.noreply.github.com>
2024-03-05 10:21:08 +0530

    Add files via upload

---
+++ -/index.js
@@ -0,0 +1,10 @@
+function on(){
+    let bulbobj=document.querySelector(".bulboff");
+    bulbobj.setAttribute("src","bulb1.png");
+
+}
+function off(){
+    let bulbobj=document.querySelector(".bulboff");
+    bulbobj.setAttribute("src","bulb2.png");
+
+}
